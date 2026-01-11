2fa73cd6db6dcb61525c8c7d0157847115e5bfb0
Alessio Piccoli <alepic@geckoblu.net>
2026-01-11 22:42:35 +0100

    pulgin __init__

--- src/mark2/plugins/__init__.py
+++ src/mark2/plugins/__init__.py
@@ -3,3 +3,20 @@
 This package contains custom plugins that extend markdown-it functionality,
 including footnote handling, heading ID generation, and YAML frontmatter parsing.
 """
+
+from mark2.plugins.container_plugin import container_plugin
+
+# from mark2.plugins.footnote_plugin import footnote_tail
+from mark2.plugins.headingsid_plugin import headingsid_plugin
+from mark2.plugins.myst_role_plugin import myst_role_plugin
+from mark2.plugins.pagebreak_plugin import pagebreak_plugin
+from mark2.plugins.sup_plugin import sup_plugin
+
+__all__ = [
+    "container_plugin",
+    # "footnote_tail",
+    "headingsid_plugin",
+    "myst_role_plugin",
+    "pagebreak_plugin",
+    "sup_plugin",
+]
